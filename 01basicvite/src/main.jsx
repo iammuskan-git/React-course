36f22a44ad12fca9cdd8676b6ba9304bd950bbfb
iammuskan-git <muskanhamal2000@gmail.com>
2024-03-16 15:20:53 +0545

    Add React.createElement link to Google in main.jsx

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+
+const reactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank'},
+  'Visit Google from Vite React.CreateElement'
+)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ 
+    reactElement   //yeta components ya function call hunthiyo ni aba tyo jasari ni HTML object ma convert hudo raixa vane yesari call gareni document.createElement lai  
+ 
 )
