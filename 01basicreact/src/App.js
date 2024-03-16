7941838b781cf53968f26fae3a0fd98464346535
iammuskan-git <muskanhamal2000@gmail.com>
2024-03-16 09:50:09 +0545

    Add new files and update existing files

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Hello from Create-React-App  | Muskan Hamal </h1>
+  );
+}
+
+export default App;
---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  
+  return (
+    <h1>Hello from Vite React App  | Muskan Hamal </h1>
+  )
+}
+
+export default App
