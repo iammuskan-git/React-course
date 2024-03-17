fee12c9bbcee338299878362e988ef9658eb7d47
iammuskan-git <muskanhamal2000@gmail.com>
2024-03-17 12:16:28 +0545

    Hooks Example for counter Add React and Vite configuration files and initial code for Counter app

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  //Hooks Example
+  const [counter, setCounter] = useState(15);
+
+  const addValue = () => {
+    if(counter < 20){
+      setCounter(counter + 1)
+    }
+  }
+
+  const removeValue = () => {
+    if(counter > 0){
+      setCounter(counter - 1)
+    }
+  }
+
+  return (
+    <>
+      <h1> Counter using UseState Hooks</h1>
+      <h2> Counter: {counter} </h2>
+      <button onClick={addValue}>Add Value {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove Value {counter}</button>
+    </>
+  )
+}
+
+export default App
