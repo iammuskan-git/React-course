8c84a40d35d85d6a0f5876c66092b560cd4c159f
iammuskan-git <muskanhamal2000@gmail.com>
2024-03-20 11:59:14 +0545

    Another easiest way of react routing

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -2,37 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements , Route} from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 
-const router = createBrowserRouter([ //nesting routes Home vitra about us, contact us rakhexam yeta
-  {
-    path: '/',
-    element: <Layout  />,
-    children: [
-      { 
-        path: "",             // "/" ma janxa tyo home ho
-        element: <Home />
-      },
-      {
-        path: "about",        // "/about" ma janxa
-        element: <About />
-      },
-      {
-        path: "contact",       // "/contact" ma janxa
-        element: <Contact />
-      }
-    ]
-  },
+// const router = createBrowserRouter([ //nesting routes Home vitra about us, contact us rakhexam yeta
+//   {
+//     path: '/',
+//     element: <Layout  />,
+//     children: [
+//       { 
+//         path: "",             // "/" ma janxa tyo home ho
+//         element: <Home />
+//       },
+//       {
+//         path: "about",        // "/about" ma janxa
+//         element: <About />
+//       },
+//       {
+//         path: "contact",       // "/contact" ma janxa
+//         element: <Contact />
+//       }
+//     ]
+//   },
 
-])
+// ])
 
-
-
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />} >
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
