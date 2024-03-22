7e34336ad1ba81c59c3bfbd96f0d3842eac0db55
iammuskan-git <muskanhamal2000@gmail.com>
2024-03-22 12:14:04 +0545

    final done of theme switcher

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,20 +1,54 @@
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./contexts/theme"
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 function App() {
   
+////// yeta dekhi 
+        const [themeMode, setThemeMode] = useState("light");
+
+        const darkTheme = () => {
+          setThemeMode("dark");
+        }
+
+        const lightTheme = () => {
+          setThemeMode("light");
+        }
+
+
+        useEffect(() => {
+          document.querySelector("html").classList.remove("dark", "light");   // suru ma sab remove gareko
+          document.querySelector("html").classList.add(themeMode)              // ani data pass garni setThemeMode ma j xa tei add hunxa ThemeMode ma
+        } , [themeMode]);
+////// yeta samma khyal garni
+
+
 
   return (
     
-<div className="flex flex-wrap min-h-screen items-center">
+    
+    <ThemeProvider  value={ { themeMode, darkTheme, lightTheme } }>    {/* yo yo value use garna pauxa tala ko components le */}
+
+
+         <div className="flex flex-wrap min-h-screen items-center">
             <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        { /* themeBth */ }
+
+                        <ThemeBtn />                                                    {/* ThemeBtn Components */}
+
                           </div>
 
                     <div className="w-full max-w-sm mx-auto">
-                        { /* cardBtn */ }
+
+                        <Card />                                                        {/* Card Components */}
+                        
                           </div>
             </div>
-</div>
+        </div>
+
+
+    </ThemeProvider>
 
   )
 }
--- 09themeswitcher/src/contexts/theme.js
+++ 09themeswitcher/src/contexts/theme.js
@@ -3,13 +3,13 @@ import { createContext, useContext } from 'react';
 
 export const ThemeContext = createContext({
     themeMode: "light",                 // useState nai use garnu parxa bhanni xaina yeta
-    darkTTheme: () => {},                 // paila hamle afai data pass login bata garethiyem 
+    darkTheme: () => {},                 // paila hamle afai data pass login bata garethiyem 
     lightTheme: () => {}                 // aba yeta chai directly global variable ma { } object ma data xa
 })
 
 export const ThemeProvider = ThemeContext.Provider;   // Provider
 
-// yo custom hooks ho function nai ta ho custom hooks
-export default function useTheme(){
+                                    // yo custom hooks ho function nai ta ho custom hooks
+export function useTheme(){
     return useContext(ThemeContext);// harek choti harek login, profile ma import useContext(UserContext) garnu pardaina
 }      
